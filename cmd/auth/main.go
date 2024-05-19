7c28bbe70707ce51c4dd9f16aeda7281468b0667
mursalbekov1 <mursalbekov.merey@gmail.com>
2024-05-19 07:34:58 +0500

    enable logger

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"auth/internal/config"
-	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println((cfg))
+	log := setupLogger(cfg.Env)
 
-	// TODO: инициалиризовать логгер
+	log.Info("starting application")
+}
 
-	// TODO: инициалиризовать app3
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
